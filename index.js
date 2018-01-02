7ce4f16838d72aa3c108c4e56327d0f6d5125286
Ben Stein <ben@jammerware.com>
2018-01-02 09:39:17 -0500

    Refactored twilio send numbers into .env

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const MessageBodyService = require('./lib/service.messageBody');
 const TwilioService = require('./lib/service.twilio');
 
 let messageBodyService = new MessageBodyService();
-let twilioService = new TwilioService(process.env.TWILIO_SID, process.env.TWILIO_AUTHTOKEN, '+18563693283');
+let twilioService = new TwilioService(process.env.TWILIO_SID, process.env.TWILIO_AUTHTOKEN, process.env.TWILIO_SENDFROM);
 
 (async () => {
-    let message = await twilioService.send('+13026702550', "Dingdog is online!");
+    let message = await twilioService.send(process.env.TWILIO_SENDTO, "Dingdog is online!");
     process.stdout.write("Message sid");
     process.stdout.write(message.sid);
 })();
